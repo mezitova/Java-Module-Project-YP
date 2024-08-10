54260bbaf5cd72ba81176c9267a96d8bf4089482
Aisylu Mezitova <amezitova@yandex.ru>
2024-08-10 21:54:05 +0300

    Убрала ненужное переопределение toString

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,13 +2,6 @@ public class Car {
     String name;
     double speed;
 
-    @Override
-    public String toString() {
-        return "Car{" +
-                "name='" + name + '\'' +
-                ", speed=" + speed +
-                '}';
-    }
 
     public Car(String name, double speed) {
         this.name = name;
